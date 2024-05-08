72ad9cbcbca3823bf8913467023824eefcbe582d
krisna-dev-07 <prasadkrishna25102002@gmail.com>
2024-05-08 13:20:17 +0530

    Initial Commit

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const conf={
+    appwriteurl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteprojectID:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwritecollectionID:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwritebucketID:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+1
+export default conf
